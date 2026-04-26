967921fe178ff2a3e9df9aa2d03b19578e91d412
Ivan A. Filatoff <filatof@gmail.com>
2026-04-26 11:47:03 +0300

    add infrastructure for gcloud

---
+++ infra/gcp_servers/03_n8n.tf
@@ -0,0 +1,77 @@
+resource "google_compute_instance" "n8n" {
+  boot_disk {
+    auto_delete = true
+    device_name = "n8n"
+
+    initialize_params {
+      image = "projects/ubuntu-os-cloud/global/images/family/ubuntu-2604-lts-amd64"
+      size  = 40
+      type  = "pd-balanced"
+    }
+
+    mode = "READ_WRITE"
+  }
+
+  can_ip_forward      = false
+  deletion_protection = false
+  enable_display      = false
+
+  labels = {
+    goog-ec-src = "vm_n8n"
+  }
+
+  machine_type = "e2-custom-medium-1024"
+
+  metadata = {
+  ssh-keys = "fill:ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOlPFhFwKepToM3D/5wgUfFsPsv99sZkfUr9gnuhYYr/ fill@MacBookAir.local"
+}
+
+  name = "n8n"
+
+
+  network_interface {
+    access_config {
+      network_tier = "PREMIUM"
+    }
+
+    queue_count = 0
+    stack_type  = "IPV4_ONLY"
+    subnetwork  = "projects/staging-492617/regions/europe-central2/subnetworks/default"
+  }
+
+  reservation_affinity {
+    type = "NO_RESERVATION"
+  }
+
+  scheduling {
+    automatic_restart   = false
+    on_host_maintenance = "TERMINATE"
+    preemptible = true
+    #provisioning_model  = "SPOT"
+  }
+
+  service_account {
+    email  = "1087283053554-compute@developer.gserviceaccount.com"
+    scopes = ["https://www.googleapis.com/auth/devstorage.read_only", "https://www.googleapis.com/auth/logging.write", "https://www.googleapis.com/auth/monitoring.write", "https://www.googleapis.com/auth/service.management.readonly", "https://www.googleapis.com/auth/servicecontrol", "https://www.googleapis.com/auth/trace.append"]
+  }
+
+  shielded_instance_config {
+    enable_integrity_monitoring = true
+    enable_secure_boot          = false
+    enable_vtpm                 = true
+  }
+
+  zone = "europe-central2-c"
+}
+
+output "n8n_internal_ip" {
+
+  value = google_compute_instance.n8n.network_interface[0].network_ip
+
+}
+
+output "n8n_external_ip" {
+
+  value = google_compute_instance.n8n.network_interface[0].access_config[0].nat_ip
+
+}
