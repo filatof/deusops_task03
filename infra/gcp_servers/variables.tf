e271f22a041f6248b205a004909bcb2c7fdd1c8f
Ivan A. Filatoff <filatof@gmail.com>
2026-05-01 11:04:14 +0300

    add dns.tf for infrastructure

---
+++ infra/gcp_servers/variables.tf
@@ -0,0 +1,11 @@
+ variable cloud_id {
+   type        = string
+   default     = "b1givfjnecaq6gsd91ml"
+   description = "cloud id"
+ }
+
+ variable folder_id {
+   type        = string
+   default     = "b1g7qh7t0i4sftogmaue"
+   description = "folder id"
+ }
