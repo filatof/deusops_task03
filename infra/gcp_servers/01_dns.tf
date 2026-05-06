d9ebbdfa91f73c97405cf077588fe6726fd0101b
Ivan A. Filatoff <filatof@gmail.com>
2026-05-06 15:49:36 +0300

    add dns record for wiki

--- infra/gcp_servers/01_dns.tf
+++ infra/gcp_servers/01_dns.tf
@@ -6,3 +6,12 @@ resource "cloudflare_record" "web_dns" {
   ttl     = 1
   proxied = true
 }
+
+resource "cloudflare_record" "web_dns_wiki" {
+  zone_id = var.cloudflare_zone_id
+  name    = "wiki"
+  content   = google_compute_instance.bastion.network_interface[0].access_config[0].nat_ip
+  type    = "A"
+  ttl     = 1
+  proxied = true
+}
